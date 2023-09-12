22adbb962ae1005450a0b442fc6ae0ca243ee5f7
Daniel Ring <DanielKRing1@gmail.com>
2023-09-11 18:33:10 -0700

    Fix CircleLineRenderer Use relative space + complete circle

--- Assets/Scripts/Visual/CircleLineRenderer.cs
+++ Assets/Scripts/Visual/CircleLineRenderer.cs
@@ -7,14 +7,22 @@ public class CircleLineRenderer
     public CircleLineRenderer(GameObject go)
     {
         this.lineRenderer = go.AddComponent<LineRenderer>();
+        this.lineRenderer.useWorldSpace = false;
     }
 
-    public void Draw(int vertexCount, float width, float yPos, float radius, Color startColor, Color endColor)
+    public void Draw(
+        int vertexCount,
+        float width,
+        float yPos,
+        float radius,
+        Color startColor,
+        Color endColor
+    )
     {
         // 1. Set color
         this.lineRenderer.startColor = startColor;
         this.lineRenderer.endColor = endColor;
-        
+
         // 2. Set width
         this.lineRenderer.startWidth = width;
         this.lineRenderer.endWidth = width;
@@ -27,13 +35,16 @@ public class CircleLineRenderer
 
     private Vector3[] CreateOutlineVertices(int vertexCount, float yPos, float radius)
     {
-        Vector3[] vertices = new Vector3[vertexCount + 1];
+        Vector3[] vertices = new Vector3[vertexCount];
 
-        for (int i = 0; i <= vertexCount; i++)
+        float angle;
+        float x;
+        float z;
+        for (int i = 0; i < vertexCount; i++)
         {
-            float angle = (float)i / vertexCount * 360f;
-            float x = radius * Mathf.Cos(Mathf.Deg2Rad * angle);
-            float z = radius * Mathf.Sin(Mathf.Deg2Rad * angle);
+            angle = (float)i / (vertexCount - 1) * 360f;
+            x = radius * Mathf.Cos(Mathf.Deg2Rad * angle);
+            z = radius * Mathf.Sin(Mathf.Deg2Rad * angle);
             vertices[i] = new Vector3(x, yPos, z);
         }
 
